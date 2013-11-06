a896b0a7a47e60fd1012a0a28cd91eb493052861
daniel@linkfactory.dk <daniel@linkfactory.dk@065657a2-4800-482f-a053-7f63772631b3>
2013-11-06 13:36:51 +0000

    Import current version from newdevprod + resolv of ticket #1340 + versionbump
    git-svn-id: https://svn.nfit.au.dk/svn/AU-CMS/extensions/globalcontent/trunk@4219 065657a2-4800-482f-a053-7f63772631b3

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,14 +1,14 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "globalcontent".
-#
-# Auto generated 25-11-2011 13:04
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "globalcontent".
+ *
+ * Auto generated 11-09-2013 14:53
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Global Content',
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.0.10',
+	'version' => '1.0.12',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => 'a:33:{s:9:"ChangeLog";s:4:"b418";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"99c5";s:14:"ext_tables.php";s:4:"dcf2";s:14:"ext_tables.sql";s:4:"142d";s:13:"locallang.xml";s:4:"230e";s:16:"locallang_db.xml";s:4:"fd00";s:7:"tca.php";s:4:"e8ab";s:14:"doc/manual.sxw";s:4:"2788";s:19:"doc/wizard_form.dat";s:4:"8b11";s:20:"doc/wizard_form.html";s:4:"eaeb";s:29:"hooks/class.globalcontent.php";s:4:"692d";s:32:"hooks/class.logGlobalContent.php";s:4:"1d14";s:42:"hooks/class.tx_globalcontent_cacheProc.php";s:4:"698b";s:60:"hooks/class.tx_globalcontent_tcemain_processDatamapClass.php";s:4:"38ec";s:42:"hooks/class.tx_globalcontent_userfuncs.php";s:4:"eb6d";s:20:"hooks/getcontent.php";s:4:"7a02";s:14:"pi1/ce_wiz.gif";s:4:"d41d";s:34:"pi1/class.tx_globalcontent_pi1.php";s:4:"93a2";s:42:"pi1/class.tx_globalcontent_pi1_wizicon.php";s:4:"6cfd";s:13:"pi1/clear.gif";s:4:"d41d";s:17:"pi1/locallang.xml";s:4:"d41d";s:24:"pi1/static/editorcfg.txt";s:4:"d41d";s:31:"pi2/class.getContentElement.php";s:4:"2799";s:34:"pi2/class.tx_globalcontent_pi2.php";s:4:"8b3c";s:17:"pi2/locallang.xml";s:4:"9260";s:24:"pi2/static/editorcfg.txt";s:4:"adff";s:28:"res/icons/network-server.png";s:4:"0778";s:20:"static/constants.txt";s:4:"d41d";s:16:"static/setup.txt";s:4:"e66c";s:35:"static/global_content/constants.txt";s:4:"d41d";s:31:"static/global_content/setup.txt";s:4:"a6c0";}',
+	'_md5_values_when_last_written' => 'a:33:{s:9:"ChangeLog";s:4:"f3f4";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"99c5";s:14:"ext_tables.php";s:4:"6561";s:14:"ext_tables.sql";s:4:"142d";s:13:"locallang.xml";s:4:"230e";s:16:"locallang_db.xml";s:4:"fd00";s:10:"README.txt";s:4:"ee2d";s:7:"tca.php";s:4:"e8ab";s:14:"doc/manual.sxw";s:4:"2788";s:19:"doc/wizard_form.dat";s:4:"8b11";s:20:"doc/wizard_form.html";s:4:"eaeb";s:29:"hooks/class.globalcontent.php";s:4:"692d";s:32:"hooks/class.logGlobalContent.php";s:4:"1d14";s:42:"hooks/class.tx_globalcontent_cacheProc.php";s:4:"698b";s:60:"hooks/class.tx_globalcontent_tcemain_processDatamapClass.php";s:4:"38ec";s:42:"hooks/class.tx_globalcontent_userfuncs.php";s:4:"eb6d";s:20:"hooks/getcontent.php";s:4:"7a02";s:14:"pi1/ce_wiz.gif";s:4:"d41d";s:34:"pi1/class.tx_globalcontent_pi1.php";s:4:"7362";s:42:"pi1/class.tx_globalcontent_pi1_wizicon.php";s:4:"6cfd";s:13:"pi1/clear.gif";s:4:"d41d";s:17:"pi1/locallang.xml";s:4:"d41d";s:24:"pi1/static/editorcfg.txt";s:4:"d41d";s:31:"pi2/class.getContentElement.php";s:4:"d163";s:34:"pi2/class.tx_globalcontent_pi2.php";s:4:"8b3c";s:17:"pi2/locallang.xml";s:4:"9260";s:24:"pi2/static/editorcfg.txt";s:4:"adff";s:28:"res/icons/network-server.png";s:4:"0778";s:20:"static/constants.txt";s:4:"d41d";s:16:"static/setup.txt";s:4:"e66c";s:35:"static/global_content/constants.txt";s:4:"d41d";s:31:"static/global_content/setup.txt";s:4:"a6c0";}',
 );
 
 ?>
--- pi2/class.getContentElement.php
+++ pi2/class.getContentElement.php
@@ -35,7 +35,7 @@ require_once('typo3/sysext/css_styled_content/pi1/class.tx_cssstyledcontent_pi1.
 
 
 if(t3lib_extMgm::isLoaded('perfectlightbox')){
-	@include_once(t3lib_extMgm::extPath('perfectlightbox').'class.tx_perfectlightbox.php');
+    @include_once(t3lib_extMgm::extPath('perfectlightbox').'class.tx_perfectlightbox.php');
 }
 
 #### PREPARE INSTANCE OF TSFE ####
@@ -68,128 +68,133 @@ $TSFE->getConfigArray();
 
 
 class globalcontent{
-	
-	function main(){
-		$elemId = intval(t3lib_div::_GP('elementId'));
+    
+    function main(){
+        $elemId = intval(t3lib_div::_GP('elementId'));
                 $content = '';
-		if($_GET['stage'] =='get'){
+        if($_GET['stage'] =='get'){
                         $record = $GLOBALS['TYPO3_DB']->exec_SELECTgetRows('*','tt_content','uid='.$elemId);
-			if($record[0]['CType'] == 'templavoila_pi1'){
-				// find all tt_content within the templavoila
-				preg_match_all('/\<value.*\>(\d+)\<\/value\>/',$record[0]['tx_templavoila_flex'],$matches);
+            if($record[0]['CType'] == 'templavoila_pi1'){
+                // find all tt_content within the templavoila
+                preg_match_all('/\<value.*\>(\d+)\<\/value\>/',$record[0]['tx_templavoila_flex'],$matches);
 
-				$cids = implode(',',$matches[1]);
-				$records = $GLOBALS['TYPO3_DB']->exec_SELECTgetRows('*','tt_content',"uid IN($cids)");
-				$records[] = $record[0];
-				
-			} else {
-				$records = $record;
-			}
-			
-			foreach($records as $id => $record){
-				preg_match_all('/<link (\d+)([^\>]*)\>/',$record['bodytext'],$matches);
-				if(count($matches[0])){
-					foreach($matches[0] as $i => $link){					
-						if(strpos($matches[2][$i],'external')===false){							
-							$tmp = t3lib_div::getUrl('http://'.$this->getDomainForPage($matches[1][$i]).'/index.php?id='.$matches[1][$i].'&type=8001');
-							$replace[$matches[0][$i]] = '<link '.$tmp.' - external-link>';
-						}
-					}
-				}
-				if(count($replace)){
-					$records[$id]['bodytext'] = str_replace(array_keys($replace),$replace,$record['bodytext']);
-				}
-				$records[$id] = array_map(utf8_encode,$records[$id]);
-			}
-			
+                $cids = implode(',',$matches[1]);
+                $records = $GLOBALS['TYPO3_DB']->exec_SELECTgetRows('*','tt_content',"uid IN($cids)");
+                $records[] = $record[0];
+                
+            } else {
+                $records = $record;
+            }
+            
+            foreach($records as $id => $record){
+                preg_match_all('/<link (\d+)([^\>]*)\>/',$record['bodytext'],$matches);
+                if(count($matches[0])){
+                    foreach($matches[0] as $i => $link){                    
+                        if(strpos($matches[2][$i],'external')===false){                            
+                            $tmp = t3lib_div::getUrl('http://'.$this->getDomainForPage($matches[1][$i]).'/index.php?id='.$matches[1][$i].'&type=8001');
+                            $replace[$matches[0][$i]] = '<link '.$tmp.' - external-link>';
+                        }
+                    }
+                }
+                if(count($replace)){
+                    $records[$id]['bodytext'] = str_replace(array_keys($replace),$replace,$record['bodytext']);
+                }
+                $records[$id] = array_map(utf8_encode,$records[$id]);
+            }
+            
                         $content = json_encode($records);
 
 
-		} elseif(intval($elemId)){
-			$cObj = t3lib_div::makeInstance("tslib_cObj");
-			if($_GET['L']>0){
-	                        $record = $GLOBALS['TYPO3_DB']->exec_SELECTgetRows('*','tt_content','l18n_parent='.$elemId.' AND sys_language_uid=' . intval($_GET['L']) . ' AND NOT deleted AND NOT hidden');
-	                } else {
-	                	$record = $GLOBALS['TYPO3_DB']->exec_SELECTgetRows('*','tt_content','uid='.$elemId.' AND NOT deleted AND NOT hidden');
-	                }
-			if(count($record[0])>1){/*go on*/} else {return false;}
+        } elseif(intval($elemId)){
+            $cObj = t3lib_div::makeInstance("tslib_cObj");
+            if($_GET['L']>0){
+                            $record = $GLOBALS['TYPO3_DB']->exec_SELECTgetRows('*','tt_content','l18n_parent='.$elemId.' AND sys_language_uid=' . intval($_GET['L']) . ' AND NOT deleted AND NOT hidden');
+                    } else {
+                        $record = $GLOBALS['TYPO3_DB']->exec_SELECTgetRows('*','tt_content','uid='.$elemId.' AND NOT deleted AND NOT hidden');
+                    }
+            if(count($record[0])>1){/*go on*/} else {return false;}
 
-                	$contentHook =  t3lib_div::makeInstance("tx_siabscripts_contenthooks");
-                	$tmpObj->content = $record[0]['bodytext'];
-			$contentHook->findAndReplace($record[0]['bodytext'],$tmpObj);
-			$record[0]['bodytext'] = $tmpObj->content;
-			// convert all internal links to external for consumer
-			preg_match_all('/\<link (\d+)([^\>]*)\>/',$record[0]['bodytext'],$matches);			
-			if(count($matches[0])){				
-				foreach($matches[0] as $i => $link){					
-					if(strpos($matches[2][$i],'external')===false){						
-						$tmp = t3lib_div::getUrl('http://'.$this->getDomainForPage($matches[1][$i]).'/index.php?id='.$matches[1][$i].'&type=8001');
-						if(strlen(trim($tmp)) && strpos($tmp,'<head') === false){
-							$replace[$matches[0][$i]] = '<link '.$tmp.' '.(str_replace('internal-link','',$matches[2][$i])).'>';
-						} elseif(strlen(trim($tmp))<1){
-							// check for mop
-							$mop = $GLOBALS['TYPO3_DB']->exec_SELECTgetRows('uid,pid,mount_pid','pages','uid='.intval($matches[1][$i]).' AND mount_pid > 0 AND deleted = 0');
-							if($mop[0]['mount_pid'] > 0){
-								$tmp = t3lib_div::getUrl('http://'.$this->getDomainForPage($mop[0]['mount_pid']).'/index.php?id='.$mop[0]['mount_pid'].'&type=8001' . ($_GET['L']>0 ? '&L='.$_GET['L']:''));
-								if(strlen(trim($tmp)) && strpos($tmp,'<head') === false){
-									$replace[$matches[0][$i]] = '<link '.$tmp.' '.(str_replace('internal-link','',$matches[2][$i])).'>';
-								}
-							}
-						}
-					}
-				}
-				if(count($replace)){
-					$record[0]['bodytext'] = str_replace(array_keys($replace),$replace,$record[0]['bodytext']);
-				}
-			}
-                        
+                    $contentHook =  t3lib_div::makeInstance("tx_siabscripts_contenthooks");
+                    $tmpObj->content = $record[0]['bodytext'];
+            $contentHook->findAndReplace($record[0]['bodytext'],$tmpObj);
+            $record[0]['bodytext'] = $tmpObj->content;
+            // convert all internal links to external for consumer
+            preg_match_all('/\<link (\d+)([^\>]*)\>/',$record[0]['bodytext'],$matches);            
+            if(count($matches[0])){                
+                foreach($matches[0] as $i => $link){                    
+                    if(strpos($matches[2][$i],'external')===false){                        
+                        $tmp = t3lib_div::getUrl('http://'.$this->getDomainForPage($matches[1][$i]).'/index.php?id='.$matches[1][$i].'&type=8001');
+                        if(strlen(trim($tmp)) && strpos($tmp,'<head') === false){
+                            $replace[$matches[0][$i]] = '<link '.$tmp.' '.(str_replace('internal-link','',$matches[2][$i])).'>';
+                        } elseif(strlen(trim($tmp))<1){
+                            // check for mop
+                            $mop = $GLOBALS['TYPO3_DB']->exec_SELECTgetRows('uid,pid,mount_pid','pages','uid='.intval($matches[1][$i]).' AND mount_pid > 0 AND deleted = 0');
+                            if($mop[0]['mount_pid'] > 0){
+                                $tmp = t3lib_div::getUrl('http://'.$this->getDomainForPage($mop[0]['mount_pid']).'/index.php?id='.$mop[0]['mount_pid'].'&type=8001' . ($_GET['L']>0 ? '&L='.$_GET['L']:''));
+                                if(strlen(trim($tmp)) && strpos($tmp,'<head') === false){
+                                    $replace[$matches[0][$i]] = '<link '.$tmp.' '.(str_replace('internal-link','',$matches[2][$i])).'>';
+                                }
+                            }
+                        }
+                    }
+                }
+                if(count($replace)){
+                    $record[0]['bodytext'] = str_replace(array_keys($replace),$replace,$record[0]['bodytext']);
+                }
+            }
+            
+            if($_GET['L']>0){
+                    $GLOBALS['TSFE']->sys_language_contentOL = 'hideNonTranslated';
+                    $GLOBALS['TSFE']->sys_language_content = $_GET['L'];    
+            }
                         // create content object
-			$cObj->start($record[0], 'tt_content'); 
-		        $parsedContent = $cObj->cObjGetSingle('<tt_content',$lConf);
-		
-			// replace internal links for src and href to external for consumer
+            $cObj->start($record[0], 'tt_content'); 
+                $parsedContent = $cObj->cObjGetSingle('<tt_content',$lConf);
+        
+            // replace internal links for src and href to external for consumer
                         //$content =  str_replace('src="','src="'.t3lib_div::getIndpEnv('TYPO3_SITE_URL'),$parsedContent);
                         //$content = preg_replace('/src="([^(http:)][^"]+)/i', 'src="'.t3lib_div::getIndpEnv('TYPO3_SITE_URL').'$1', $parsedContent);
-     			$content = preg_replace('/src="(fileadmin|uploads|typo3temp|typo3conf)/i', 'src="'.t3lib_div::getIndpEnv('TYPO3_SITE_URL').'$1', $parsedContent);
+                 $content = preg_replace('/src="(fileadmin|uploads|typo3temp|typo3conf)/i', 'src="'.t3lib_div::getIndpEnv('TYPO3_SITE_URL').'$1', $parsedContent);
 
                         $content = preg_replace('/href="([^(http:)][^"]+)/i', 'href="'.t3lib_div::getIndpEnv('TYPO3_SITE_URL').'$1', $content);
 
                 }
                 print($content);
-	}
+    }
 
         
-	
+    
         /**
-	* Function to fetch the proper domains.
-	* This must be a sys_domain record from the page tree.
-	*	
-	* @param    array       Record of page to get the correct domain for.
-	* @return   string      Correct domain.
-	*/
-	function getDomainForPage($pid) {
-		/* What pages to search */
-		$pids = array_reverse(t3lib_befunc::BEgetRootLine($pid));
-		//print_r($pids);
-		foreach ($pids as $page) {
-			/* Domains: Find domain for page */			
-			$rs = $GLOBALS['TYPO3_DB']->sql_query("SELECT domainName FROM sys_domain
-			    INNER JOIN pages ON sys_domain.pid = pages.uid
-			    WHERE NOT sys_domain.hidden
-			    AND NOT pages.hidden
-			    AND NOT pages.deleted
-			    AND pages.uid = {$page['uid']}
-			    ORDER BY sys_domain.sorting
-			    LIMIT 0,1");
-			if ($GLOBALS['TYPO3_DB']->sql_num_rows($rs)) {
-				list($domain) = $GLOBALS['TYPO3_DB']->sql_fetch_row($rs);
-			}
-		}
-		return $domain;
-	}
+    * Function to fetch the proper domains.
+    * This must be a sys_domain record from the page tree.
+    *    
+    * @param    array       Record of page to get the correct domain for.
+    * @return   string      Correct domain.
+    */
+    function getDomainForPage($pid) {
+        /* What pages to search */
+        $pids = array_reverse(t3lib_befunc::BEgetRootLine($pid));
+        //print_r($pids);
+        foreach ($pids as $page) {
+            /* Domains: Find domain for page */            
+            $rs = $GLOBALS['TYPO3_DB']->sql_query("SELECT domainName FROM sys_domain
+                INNER JOIN pages ON sys_domain.pid = pages.uid
+                WHERE NOT sys_domain.hidden
+                AND NOT pages.hidden
+                AND NOT pages.deleted
+                AND pages.uid = {$page['uid']}
+                ORDER BY sys_domain.sorting
+                LIMIT 0,1");
+            if ($GLOBALS['TYPO3_DB']->sql_num_rows($rs)) {
+                list($domain) = $GLOBALS['TYPO3_DB']->sql_fetch_row($rs);
+            }
+        }
+        return $domain;
+    }
 }
 
 $run =  t3lib_div::makeInstance('globalcontent');
 $run->main();
 
 ?>
+

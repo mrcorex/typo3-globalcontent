db20e4537047e425e6f39226ed024c430cc65ac1
Carsten Jørgensen <carsten@linkfactory.dk>
2014-09-11 13:44:37 +0200

    [TASK] Moved dislaimer to README.txt and removed from files. Prepared ChangeLog for release.

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,24 +1,21 @@
 <?php
 
-
-/**
- * ************************************************************
+/***************************************************************
  * Extension Manager/Repository config file for ext "globalcontent".
  *
- * Auto generated 11-09-2013 14:53
+ * Auto generated 11-09-2014 13:42
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
  * writing. "version" and "dependencies" must not be touched!
- * *************************************************************
- */
+ ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Global Content',
 	'description' => '',
 	'category' => 'plugin',
-	'author' => 'Thor Solli',
-	'author_email' => 'thor@linkfactory.dk',
+	'author' => 'Carsten Jørgensen',
+	'author_email' => 'dev@corex.dk',
 	'shy' => '',
 	'dependencies' => '',
 	'conflicts' => '',
@@ -43,5 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => 'a:33:{s:9:"ChangeLog";s:4:"f3f4";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"99c5";s:14:"ext_tables.php";s:4:"6561";s:14:"ext_tables.sql";s:4:"142d";s:13:"locallang.xml";s:4:"230e";s:16:"locallang_db.xml";s:4:"fd00";s:10:"README.txt";s:4:"ee2d";s:7:"tca.php";s:4:"e8ab";s:14:"doc/manual.sxw";s:4:"2788";s:19:"doc/wizard_form.dat";s:4:"8b11";s:20:"doc/wizard_form.html";s:4:"eaeb";s:29:"hooks/class.globalcontent.php";s:4:"692d";s:32:"hooks/class.logGlobalContent.php";s:4:"1d14";s:42:"hooks/class.tx_globalcontent_cacheProc.php";s:4:"698b";s:60:"hooks/class.tx_globalcontent_tcemain_processDatamapClass.php";s:4:"38ec";s:42:"hooks/class.tx_globalcontent_userfuncs.php";s:4:"eb6d";s:20:"hooks/getcontent.php";s:4:"7a02";s:14:"pi1/ce_wiz.gif";s:4:"d41d";s:34:"pi1/class.tx_globalcontent_pi1.php";s:4:"7362";s:42:"pi1/class.tx_globalcontent_pi1_wizicon.php";s:4:"6cfd";s:13:"pi1/clear.gif";s:4:"d41d";s:17:"pi1/locallang.xml";s:4:"d41d";s:24:"pi1/static/editorcfg.txt";s:4:"d41d";s:31:"pi2/class.getContentElement.php";s:4:"d163";s:34:"pi2/class.tx_globalcontent_pi2.php";s:4:"8b3c";s:17:"pi2/locallang.xml";s:4:"9260";s:24:"pi2/static/editorcfg.txt";s:4:"adff";s:28:"res/icons/network-server.png";s:4:"0778";s:20:"static/constants.txt";s:4:"d41d";s:16:"static/setup.txt";s:4:"e66c";s:35:"static/global_content/constants.txt";s:4:"d41d";s:31:"static/global_content/setup.txt";s:4:"a6c0";}',
+	'_md5_values_when_last_written' => 'a:28:{s:9:"ChangeLog";s:4:"f9fd";s:16:"ext_autoload.php";s:4:"aef2";s:21:"ext_conf_template.txt";s:4:"2f9d";s:12:"ext_icon.gif";s:4:"f234";s:17:"ext_localconf.php";s:4:"a93f";s:14:"ext_tables.php";s:4:"7a9e";s:14:"ext_tables.sql";s:4:"d7cc";s:13:"locallang.xml";s:4:"fc6b";s:16:"locallang_db.xml";s:4:"c8d4";s:10:"README.txt";s:4:"6fe4";s:48:"classes/class.tx_globalcontent_configuration.php";s:4:"25cf";s:38:"classes/class.tx_globalcontent_eid.php";s:4:"0171";s:42:"classes/class.tx_globalcontent_fetcher.php";s:4:"50db";s:14:"doc/manual.sxw";s:4:"2788";s:19:"doc/wizard_form.dat";s:4:"8b11";s:20:"doc/wizard_form.html";s:4:"eaeb";s:42:"hooks/class.tx_globalcontent_cacheproc.php";s:4:"3721";s:40:"hooks/class.tx_globalcontent_tcemain.php";s:4:"2c6d";s:42:"hooks/class.tx_globalcontent_userfuncs.php";s:4:"d070";s:14:"pi1/ce_wiz.gif";s:4:"d41d";s:34:"pi1/class.tx_globalcontent_pi1.php";s:4:"b3e9";s:13:"pi1/clear.gif";s:4:"d41d";s:17:"pi1/locallang.xml";s:4:"d41d";s:31:"pi1/res/jquery.ajax_autoload.js";s:4:"e2f1";s:19:"pi1/res/spin.min.js";s:4:"f2b0";s:28:"res/icons/network-server.png";s:4:"0778";s:20:"static/constants.txt";s:4:"d41d";s:16:"static/setup.txt";s:4:"c391";}',
 );
+
+?>

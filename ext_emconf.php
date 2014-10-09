ec539b14e1379927901ac722bc35852410b1bce2
Carsten Jørgensen <carsten@linkfactory.dk>
2014-10-09 12:26:35 +0200

    [RELEASE] Bumped to version 2.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "globalcontent".
  *
- * Auto generated 11-09-2014 13:42
+ * Auto generated 09-10-2014 12:25
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'conflicts' => '',
 	'priority' => '',
 	'module' => '',
-	'state' => 'beta',
+	'state' => 'stable',
 	'internal' => '',
 	'uploadfolder' => 0,
 	'createDirs' => '',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => 'a:28:{s:9:"ChangeLog";s:4:"f9fd";s:16:"ext_autoload.php";s:4:"aef2";s:21:"ext_conf_template.txt";s:4:"2f9d";s:12:"ext_icon.gif";s:4:"f234";s:17:"ext_localconf.php";s:4:"a93f";s:14:"ext_tables.php";s:4:"7a9e";s:14:"ext_tables.sql";s:4:"d7cc";s:13:"locallang.xml";s:4:"fc6b";s:16:"locallang_db.xml";s:4:"c8d4";s:10:"README.txt";s:4:"6fe4";s:48:"classes/class.tx_globalcontent_configuration.php";s:4:"25cf";s:38:"classes/class.tx_globalcontent_eid.php";s:4:"0171";s:42:"classes/class.tx_globalcontent_fetcher.php";s:4:"50db";s:14:"doc/manual.sxw";s:4:"2788";s:19:"doc/wizard_form.dat";s:4:"8b11";s:20:"doc/wizard_form.html";s:4:"eaeb";s:42:"hooks/class.tx_globalcontent_cacheproc.php";s:4:"3721";s:40:"hooks/class.tx_globalcontent_tcemain.php";s:4:"2c6d";s:42:"hooks/class.tx_globalcontent_userfuncs.php";s:4:"d070";s:14:"pi1/ce_wiz.gif";s:4:"d41d";s:34:"pi1/class.tx_globalcontent_pi1.php";s:4:"b3e9";s:13:"pi1/clear.gif";s:4:"d41d";s:17:"pi1/locallang.xml";s:4:"d41d";s:31:"pi1/res/jquery.ajax_autoload.js";s:4:"e2f1";s:19:"pi1/res/spin.min.js";s:4:"f2b0";s:28:"res/icons/network-server.png";s:4:"0778";s:20:"static/constants.txt";s:4:"d41d";s:16:"static/setup.txt";s:4:"c391";}',
+	'_md5_values_when_last_written' => 'a:21:{s:9:"ChangeLog";s:4:"f9fd";s:16:"ext_autoload.php";s:4:"aef2";s:21:"ext_conf_template.txt";s:4:"2f9d";s:12:"ext_icon.gif";s:4:"f234";s:17:"ext_localconf.php";s:4:"a93f";s:14:"ext_tables.php";s:4:"7a9e";s:14:"ext_tables.sql";s:4:"d7cc";s:13:"locallang.xml";s:4:"fc6b";s:16:"locallang_db.xml";s:4:"c8d4";s:10:"README.txt";s:4:"6fe4";s:48:"classes/class.tx_globalcontent_configuration.php";s:4:"7a18";s:38:"classes/class.tx_globalcontent_eid.php";s:4:"a421";s:42:"classes/class.tx_globalcontent_fetcher.php";s:4:"478f";s:42:"hooks/class.tx_globalcontent_cacheproc.php";s:4:"f1c9";s:40:"hooks/class.tx_globalcontent_tcemain.php";s:4:"61c2";s:42:"hooks/class.tx_globalcontent_userfuncs.php";s:4:"1484";s:34:"pi1/class.tx_globalcontent_pi1.php";s:4:"6d8d";s:31:"pi1/res/jquery.ajax_autoload.js";s:4:"e2f1";s:19:"pi1/res/spin.min.js";s:4:"f2b0";s:20:"static/constants.txt";s:4:"d41d";s:16:"static/setup.txt";s:4:"c391";}',
 );
 
 ?>

367d7c3ee84007a2e62be807795441a013e15b6e
Carsten Jørgensen <carsten@linkfactory.dk>
2014-11-05 10:00:39 +0100

    [RELEASE] Bumped version to 2.0.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "globalcontent".
  *
- * Auto generated 09-10-2014 12:25
+ * Auto generated 05-11-2014 09:58
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Global Content',
 	'description' => '',
 	'category' => 'plugin',
-	'author' => 'Carsten JÃ¸rgensen',
+	'author' => 'Carsten Jørgensen',
 	'author_email' => 'dev@corex.dk',
 	'shy' => '',
 	'dependencies' => '',
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '2.0.0',
+	'version' => '2.0.1',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => 'a:21:{s:9:"ChangeLog";s:4:"f9fd";s:16:"ext_autoload.php";s:4:"aef2";s:21:"ext_conf_template.txt";s:4:"2f9d";s:12:"ext_icon.gif";s:4:"f234";s:17:"ext_localconf.php";s:4:"a93f";s:14:"ext_tables.php";s:4:"7a9e";s:14:"ext_tables.sql";s:4:"d7cc";s:13:"locallang.xml";s:4:"fc6b";s:16:"locallang_db.xml";s:4:"c8d4";s:10:"README.txt";s:4:"6fe4";s:48:"classes/class.tx_globalcontent_configuration.php";s:4:"7a18";s:38:"classes/class.tx_globalcontent_eid.php";s:4:"a421";s:42:"classes/class.tx_globalcontent_fetcher.php";s:4:"478f";s:42:"hooks/class.tx_globalcontent_cacheproc.php";s:4:"f1c9";s:40:"hooks/class.tx_globalcontent_tcemain.php";s:4:"61c2";s:42:"hooks/class.tx_globalcontent_userfuncs.php";s:4:"1484";s:34:"pi1/class.tx_globalcontent_pi1.php";s:4:"6d8d";s:31:"pi1/res/jquery.ajax_autoload.js";s:4:"e2f1";s:19:"pi1/res/spin.min.js";s:4:"f2b0";s:20:"static/constants.txt";s:4:"d41d";s:16:"static/setup.txt";s:4:"c391";}',
+	'_md5_values_when_last_written' => 'a:22:{s:9:"ChangeLog";s:4:"f9fd";s:16:"ext_autoload.php";s:4:"f326";s:21:"ext_conf_template.txt";s:4:"2f9d";s:12:"ext_icon.gif";s:4:"f234";s:17:"ext_localconf.php";s:4:"6487";s:14:"ext_tables.php";s:4:"7a9e";s:14:"ext_tables.sql";s:4:"2e0d";s:13:"locallang.xml";s:4:"fc6b";s:16:"locallang_db.xml";s:4:"c8d4";s:10:"README.txt";s:4:"6fe4";s:40:"classes/class.tx_globalcontent_cache.php";s:4:"c78d";s:48:"classes/class.tx_globalcontent_configuration.php";s:4:"7a18";s:38:"classes/class.tx_globalcontent_eid.php";s:4:"a421";s:42:"classes/class.tx_globalcontent_fetcher.php";s:4:"5d6f";s:42:"hooks/class.tx_globalcontent_cacheproc.php";s:4:"67db";s:40:"hooks/class.tx_globalcontent_tcemain.php";s:4:"edd9";s:42:"hooks/class.tx_globalcontent_userfuncs.php";s:4:"1484";s:34:"pi1/class.tx_globalcontent_pi1.php";s:4:"6d8d";s:31:"pi1/res/jquery.ajax_autoload.js";s:4:"e2f1";s:19:"pi1/res/spin.min.js";s:4:"f2b0";s:20:"static/constants.txt";s:4:"d41d";s:16:"static/setup.txt";s:4:"c391";}',
 );
 
 ?>

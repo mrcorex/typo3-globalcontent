e12c2cdf3ea1c2357467aa59ba9726873965324a
Carsten Jørgensen <carsten@linkfactory.dk>
2015-02-26 16:36:55 +0100

    [TASK] Added plugin in backend-list of plugins (when adding....).

--- ext_tables.php
+++ ext_tables.php
@@ -53,3 +53,8 @@ t3lib_extMgm::addStaticFile($_EXTKEY, "static/", "Global Content Page types");
 
 $TCA['tt_content']['types'][$_EXTKEY . '_pi1']['showitem'] = 'CType;;4;button;1-1-1, header;;3;;2-2-2,tx_globalcontent,tx_globalcontent_link,tx_globalcontent_orgurl,tx_globalcontent_fetcher';
 t3lib_extMgm::addPlugin(array('LLL:EXT:globalcontent/locallang_db.xml:tt_content.CType_pi1', $_EXTKEY . '_pi1'), 'CType');
+
+// Add plugin to list of plugins in backend (when adding plugin).
+if (TYPO3_MODE == 'BE')   {
+	$TBE_MODULES_EXT['xMOD_db_new_content_el']['addElClasses']['tx_globalcontent_wizicon'] = t3lib_extMgm::extPath($_EXTKEY) . 'hooks/class.tx_globalcontent_wizicon.php';
+}
---
+++ hooks/class.tx_globalcontent_wizicon.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Class that adds the wizard icon.
+ */
+class tx_globalcontent_wizicon {
+
+	/**
+	 * Adds the newloginbox wizard icon
+	 *
+	 * @param array Input array with wizard items for plugins
+	 * @return array Modified input array, having the item for newloginbox added.
+	 */
+	function proc($wizardItems)	{
+		global $LANG;
+
+		$ll = $this->includeLocalLang();
+		$wizardItems['plugins_tx_globalcontent_pi1'] = array(
+			'icon' => t3lib_extMgm::extRelPath('globalcontent') . 'res/icons/network-server.png',
+			'title' => $LANG->getLLL('pi_title', $ll),
+			'description' => $LANG->getLLL('pi_plus_wiz_description', $ll),
+			'params' => '&defVals[tt_content][CType]=globalcontent_pi1'
+		);
+
+		return $wizardItems;
+                            
+	}
+
+	/**
+	 * Includes the locallang file for the 'tt_news' extension
+	 *
+	 * @return array The LOCAL_LANG array
+	 */
+	function includeLocalLang()    {
+		if (class_exists("\\TYPO3\\CMS\\Core\\Utility\\GeneralUtility")) {
+			$LOCAL_LANG = \TYPO3\CMS\Core\Utility\GeneralUtility::readLLfile(
+				\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('globalcontent') . 'locallang.xml',
+				$GLOBALS['LANG']->lang
+			);
+		} else {
+			$llFile = t3lib_extMgm::extPath('globalcontent') . 'locallang.xml';
+			$LOCAL_LANG = t3lib_div::readLLXMLfile($llFile, $GLOBALS['LANG']->lang);
+		}
+		return $LOCAL_LANG;
+	}
+}
